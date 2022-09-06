a6221914d9374f422d4b831e28e3573f0f20209b
School <111399819+ProjectRyzhenko@users.noreply.github.com>
2022-09-06 19:52:54 +0300

    111

--- Bot/bot.jsx
+++ Bot/bot.jsx
@@ -2,10 +2,10 @@ const { Telegraf } = require("telegraf");
 const TOKEN = "5732265646:AAE5aqq3BCa4CNLjS2soFLjQ2s0h2nZms-w";
 const bot = new Telegraf(TOKEN);
 
-const web_link = "https://63176b4d9c15680008aadcc2--admirable-cucurucho-26aa0b.netlify.app/";
+const web_link = "https://unique-vacherin-86609e.netlify.app/";
 
 bot.start((ctx) =>
-  ctx.reply("Welcome  Добро пожаловать:))", {
+  ctx.reply("Welcome:))", {
     reply_markup: {
       keyboard: [[{ text :'click' , web_app: { url: web_link } }]],
     },
