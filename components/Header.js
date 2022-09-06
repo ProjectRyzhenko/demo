230be3e2979de6962f5c154cfb8526d0401606a0
School <111399819+ProjectRyzhenko@users.noreply.github.com>
2022-09-06 19:43:01 +0300

    1111

--- components/Header.js
+++ components/Header.js
@@ -7,7 +7,8 @@ export default function Header({ name }) {
   return (
     <header className="pt-20 pb-12">
 
-           
+    <img src='https://cdn.pixabay.com/photo/2022/08/31/19/10/ukraine-7424066_960_720.png'/>
+      
 
       <p className="text-2xl dark:text-white text-center">
         <Link href="/">
